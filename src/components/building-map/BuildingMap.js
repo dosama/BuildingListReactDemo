2ce197b6625c1dc4b1dc6a24f5be22123ea27f76
Dina Osama <dina.osama88@gmail.com>
2020-10-22 19:08:00 +0200

    Add Google Map

--- src/components/building-map/BuildingMap.js
+++ src/components/building-map/BuildingMap.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import './BuildingMap.css';
+import React, { useEffect, useRef } from 'react'
+import config from "../../config";
 
-function BuildingMap() {
+export default function BuildingMap({ options, onMount, className }) {
+  const props = { ref: useRef(), className }
+  const onLoad = () => {
+    const map = new window.google.maps.Map(props.ref.current, options)
+    onMount && onMount(map)
+  }
+
+  useEffect(() => {
+    if (!window.google) {
+      const script = document.createElement(`script`)
+      script.type = `text/javascript`;
+      script.src = config.GOOGLE_MAPS_API;
+
+      const headScript = document.getElementsByTagName(`script`)[0];
+      headScript.parentNode.insertBefore(script, headScript);
+      script.addEventListener(`load`, onLoad);
+      return () => script.removeEventListener(`load`, onLoad);
+    } else onLoad();
+  })
+
   return (
-
-  <div  className="BuildingMap">
-  
-Building Map
- </div>
-
- );
+    <div {...props} style={{height: `70vh` ,margin: `1em 0`, borderRadius: `0.5em` }}/>
+  )
 }
 
-export default BuildingMap;
+BuildingMap.defaultProps = {
+  options: {
+    center: { lat: 48, lng: 8 },
+    zoom: 5,
+  }
+}
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-      url: ""
+  GOOGLE_MAPS_API: "https://maps.google.com/maps/api/js?key=AIzaSyDBRL-reLiU_KNoxqW939mAIOOhwoYyUvw"
   };
