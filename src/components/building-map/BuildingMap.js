7d64c9c4f03ab6aa6e85be16845531bf401d0d85
Dina Osama <dina.osama88@gmail.com>
2020-10-23 10:46:43 +0200

    Add Map Spinner

--- src/components/building-map/BuildingMap.js
+++ src/components/building-map/BuildingMap.js
@@ -1,55 +1,75 @@
-import React, { useState,useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import config from "../../config";
 import buildings from '../../data/buildings.json';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import Spinner from '../spinner/Spinner';
 
 function BuildingMap() {
   const [building, setBuilding] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [map, setMap] = useState(null);
+
   const params = useParams();
   const props = { ref: useRef() }
 
-  useEffect(() => { 
-    if(!window.google)
-    {
-      const script = document.createElement(`script`)
-      script.type = `text/javascript`;
-      script.src = config.GOOGLE_MAPS_API;
-      const headScript = document.getElementsByTagName(`script`)[0];
-      headScript.parentNode.insertBefore(script, headScript);
-      script.addEventListener(`load`, refreshMap);
-      return () => script.removeEventListener(`load`, refreshMap);
-    }else{
-      refreshMap();  
-    }
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => refreshMap(), 1000);
   }, [building]);
 
   useEffect(() => {
-    refreshMap();
+    setIsLoading(true);
+    setTimeout(() => {
+      if (!window.google) {
+        const script = document.createElement(`script`)
+        script.type = `text/javascript`;
+        script.src = config.GOOGLE_MAPS_API;
+        const headScript = document.getElementsByTagName(`script`)[0];
+        headScript.parentNode.insertBefore(script, headScript);
+        script.addEventListener(`load`, refreshMap);
+        return () => script.removeEventListener(`load`, refreshMap);
+      } else {
+        refreshMap();
+      }
+    }, 1000);
+
   }, [params.buildingId]);
 
-  const refreshMap = () => {
-    const currentBuilding = buildings.find((o)=> o.id == params.buildingId);
-    setBuilding(currentBuilding?currentBuilding:{});
 
-    if(building)
-    {
-      console.log(building);
-      const myLatLng = { lat: building.location.position[0], lng: building.location.position[1]};
+  const refreshMap = () => {
+    const currentBuilding = buildings.find((o) => o.id == params.buildingId);
+    if (building && building.id == currentBuilding.id && map) {
+      return;
+    }
+
+    setBuilding(currentBuilding ? currentBuilding : null);
+
+    if (building) {
+      console.log('intialize map');
+      const myLatLng = { lat: building.location.position[0], lng: building.location.position[1] };
+
       const map = new window.google.maps.Map(props.ref.current, {
         zoom: 10,
         center: myLatLng,
       });
+
+      setMap(map);
+      window.google.maps.event.addListenerOnce(map, 'idle', function () {
+        setIsLoading(false);
+      });
+
       new window.google.maps.Marker({
         position: myLatLng,
         map,
         title: building.location.name,
       });
     }
-   
   }
+  return (<div className="BuildingMap">
+    { isLoading ? <Spinner /> : null}
+    <div {...props} style={{ visibility: isLoading ? `hidden` : `visible`, height: `100vh`, margin: `1em 0`, borderRadius: `0.5em` }} />
+  </div>
+  );
 
-  return (
-    <div {...props} style={{height: `70vh` ,margin: `1em 0`, borderRadius: `0.5em` }}/>
-  )  
 }
 export default BuildingMap;
